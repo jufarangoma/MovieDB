390ab06a28e9cdb6a4f6d2822909994c2ab9756b
jufarangoma <jufarangoma@unal.edu.co>
2018-03-29 21:33:47 -0500

    Look and feel of the MovieDB

---
+++ app/src/main/java/com/example/root/moviedb/Models/Movie.kt
@@ -0,0 +1,12 @@
+package com.example.root.moviedb.Models
+
+import android.databinding.DataBindingUtil
+import io.realm.RealmObject
+
+/**
+* Created by Juan Arango on 3/29/18.
+*/
+
+class Movie: RealmObject() {
+
+}
