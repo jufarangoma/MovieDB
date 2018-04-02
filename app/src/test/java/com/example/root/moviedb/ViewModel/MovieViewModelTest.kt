d05aa69c549a0b2b430e8ace543481b26bfc044a
jufarangoma <juanfe36@hotmail.com>
2018-04-01 23:41:24 -0500

    Test Implementation

--- app/src/test/java/com/example/root/moviedb/ViewModel/MovieViewModelTest.kt
+++ app/src/test/java/com/example/root/moviedb/ViewModel/MovieViewModelTest.kt
@@ -1,46 +1,51 @@
 package com.example.root.moviedb.ViewModel
 
+import android.content.Context
+import android.test.mock.MockApplication
+import android.support.test.InstrumentationRegistry.getContext
+import com.example.root.moviedb.Models.Movie
+import com.example.root.moviedb.Utils.Constants
+import com.example.root.moviedb.Views.Activity.MovieActivity
+import io.reactivex.Observable
 import io.realm.Realm
 import org.junit.After
 import org.junit.Test
-
-import org.junit.Assert.*
 import org.junit.Before
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
+import java.util.*
 
 /**
  * Created by Juan Arango on 4/1/18.
  */
 class MovieViewModelTest {
+    @Mock
+    val movieViewMock: MovieActivity = mock(MovieActivity::class.java)
+    var movieViewModel: MovieViewModel?=null
+    val querie: HashMap<String, String> = HashMap()
+    @Mock var context: Context?=MovieActivity()
+
 
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        //Realm.init(this)
+        Realm.init(getContext())
+        querie.put(Constants.Key.API_KEY, Constants.Value.API_KEY)
+        movieViewModel = MovieViewModel(getContext(), Constants.Url.DISCOVER,querie)
+    }
+
+    @Test
+    fun testMovieApi() {
+        doReturn(Observable.just(Movie::class.java))
+                .`when`(movieViewModel)!!
+                .fetchMoviesList(Constants.Url.DISCOVER,querie)
+
     }
 
     @After
     fun tearDown() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    @Test
-    fun fetchMoviesList() {
-    }
-
-    @Test
-    fun updateMovieList() {
-    }
-
-    @Test
-    fun onFailureRequest() {
-    }
-
-    @Test
-    fun getMovieList() {
-    }
-
-    @Test
-    fun reset() {
+        movieViewModel
     }
 }
